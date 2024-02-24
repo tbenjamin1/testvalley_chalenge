a77b61daf60e79c0143827315eb2cfd96d579e16
TUYISINGIZE Benjamin <tbenpollyl@gmail.com>
2024-02-25 01:08:39 +0200

    main shortcut

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Navbar from "@/containers/Navbar";
 import Banner from "@/containers/Banner";
 import AllItems from "@/containers/AllItems";
 import HotDeals from "@/containers/HotDeals";
-// import MainShortcut from "@/containers/MainShortCut";
+import MainShortcut from "@/containers/MainShortCut";
+
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
     <main className="flex min-h-screen  flex-col items-center main-swipper-container">
       <Navbar />
       <Banner />
-      {/* <MainShortcut /> */}
+      <MainShortcut/>
       <HotDeals />
       <AllItems />
     </main>
