98f6d1eb2a15522123df9b21e56bcb65eb976ad1
TUYISINGIZE Benjamin <tbenpollyl@gmail.com>
2024-02-25 12:57:36 +0200

    main shortcut

--- containers/Shortcut.jsx
+++ containers/Shortcut.jsx
@@ -8,7 +8,15 @@ const Shortcut = async () => {
   const response = await fetch('https://api.testvalley.kr/main-shortcut/all', { cache: 'no-store' });
   const mainShortcut = await response.json();
   return (
-    <div>Shortcut</div>
+    <div className=" short-cut-conatiner">
+
+    {mainShortcut.map((data, index) => (
+      <div key={index} className='short-cut-item' >
+        <img src={data.imageUrl} alt="" className=' w-20 h-20  cursor-pointer' />
+        <span> {data.title} </span>
+      </div>
+    ))}
+  </div>
   )
 }
 
