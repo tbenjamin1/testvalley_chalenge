e838cf5b6fecac16e36aeff8c596412c4f5dba57
TUYISINGIZE Benjamin <tbenpollyl@gmail.com>
2024-02-25 12:56:13 +0200

    main shortcut

--- containers/Shortcut.jsx
+++ containers/Shortcut.jsx
@@ -1,6 +1,12 @@
+"use client"
+
 import React from 'react'
 
-const Shortcut = () => {
+const Shortcut = async () => {
+
+
+  const response = await fetch('https://api.testvalley.kr/main-shortcut/all', { cache: 'no-store' });
+  const mainShortcut = await response.json();
   return (
     <div>Shortcut</div>
   )
