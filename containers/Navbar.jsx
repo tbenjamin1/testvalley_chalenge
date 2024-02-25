2328bf9f1852977c91460484c25324e0c9d9ddc4
TUYISINGIZE Benjamin <tbenpollyl@gmail.com>
2024-02-25 13:58:00 +0200

    responsivenesse and nav bar

--- containers/Navbar.jsx
+++ containers/Navbar.jsx
@@ -6,10 +6,13 @@ const Navbar = () => {
         <div className='w-9/12 flex items-center justify-between my-4 sub-nav-container '>
             <div className='TestValley'>
                 <span className=' valley-logo'>TestValley</span>
-                <span><img src="menuIcon.png" alt="" /></span>
+                <span className='menu-icon' ><img src="menuIcon.png" alt="" /></span>
                 <span className='category' >category</span>
             </div>
             <div className='search flex flex-row justify-center items-center border border-gray-300 p-2'>
+            <span className='notification' >
+                    <img src="notification.png" alt="" />
+                </span>
                 <span>
                     <img src="search.svg" alt="" />
                 </span>
@@ -17,7 +20,7 @@ const Navbar = () => {
                     <input type="text" className=' ' placeholder={`If you're wondering whether to buy it or not`} />
                
             </div>
-            <div className='flex flex-row gap-6'>
+            <div className='flex flex-row gap-6 login-container'>
                 <span>
                     <img src="home-event.svg" alt="" />
                 </span>
